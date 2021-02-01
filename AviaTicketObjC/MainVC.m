f073d7d8dd509661ebe8e08daedc4a3d8b7ca0b2
naigel7777 <56576758+naigel7777@users.noreply.github.com>
2021-02-02 00:59:36 +0300

    finish with date pickers
    request fir tickets work properly with date

--- AviaTicketObjC/MainVC.m
+++ AviaTicketObjC/MainVC.m
@@ -15,13 +15,14 @@
 
 @interface MainVC ()<PlaceVCDelegate>
 
-@property (nonatomic, strong) UIView *placeContainer;
+@property (nonatomic, strong) UIView *placeContainerFrom;
+@property (nonatomic, strong) UIView *placeContainerTo;
 @property (nonatomic, strong) UIButton *departureButton;
 @property (nonatomic, strong) UIButton *arrivalButton;
 @property (nonatomic, strong) UIButton *searchButton;
 @property (nonatomic, strong) UIButton *mapButton;
 @property (nonatomic, strong) UIDatePicker *fromDate;
-@property (nonatomic, strong) NSDate *fromDatePicker;
+@property (nonatomic, strong) UIDatePicker *toDate;
 @property (nonatomic) SearchRequest searchRequest;
 
 @end
@@ -40,13 +41,13 @@
  //   self.navigationController.navigationBar.backgroundColor = [UIColor systemBlueColor];
 //    self.title = @"Search";
   
-    _placeContainer = [[UIView alloc] initWithFrame:CGRectMake(20, 140, [UIScreen mainScreen].bounds.size.width - 40, 170)];
-    _placeContainer.backgroundColor = [[UIColor systemBlueColor]colorWithAlphaComponent:0.3];
-    _placeContainer.layer.shadowColor = [[[UIColor blackColor] colorWithAlphaComponent:0.3] CGColor];
-    _placeContainer.layer.shadowOffset = CGSizeZero;
-    _placeContainer.layer.shadowRadius = 20;
-    _placeContainer.layer.shadowOpacity = 1.0;
-    _placeContainer.layer.cornerRadius = 6.0;
+    _placeContainerFrom = [[UIView alloc] initWithFrame:CGRectMake(20, 140, [UIScreen mainScreen].bounds.size.width - 40, 170)];
+    _placeContainerFrom.backgroundColor = [[UIColor systemBlueColor]colorWithAlphaComponent:0.3];
+    _placeContainerFrom.layer.shadowColor = [[[UIColor blackColor] colorWithAlphaComponent:0.3] CGColor];
+    _placeContainerFrom.layer.shadowOffset = CGSizeZero;
+    _placeContainerFrom.layer.shadowRadius = 20;
+    _placeContainerFrom.layer.shadowOpacity = 1.0;
+    _placeContainerFrom.layer.cornerRadius = 6.0;
     
     
     _departureButton = [UIButton buttonWithType:UIButtonTypeSystem];
@@ -54,47 +55,70 @@
     _departureButton.tintColor = [UIColor blueColor];
     _departureButton.frame = CGRectMake(30, 20, [UIScreen mainScreen].bounds.size.width - 100.0, 60);
     _departureButton.layer.cornerRadius = 4.0;
-    _departureButton.backgroundColor = [[UIColor lightGrayColor]colorWithAlphaComponent:0.6];
+    _departureButton.backgroundColor = [[UIColor lightGrayColor]colorWithAlphaComponent:0.9];
     [_departureButton addTarget:self action:@selector(placeButtonTaped:) forControlEvents:UIControlEventTouchUpInside];
-    [self.placeContainer addSubview:_departureButton];
+    [self.placeContainerFrom addSubview:_departureButton];
     
     _fromDate = [[UIDatePicker alloc] initWithFrame:CGRectMake(30, CGRectGetMaxY(_departureButton.frame) + 20, [UIScreen mainScreen].bounds.size.width - 100.0, 60)];
+    _fromDate.backgroundColor = [[UIColor lightGrayColor]colorWithAlphaComponent:0.6];
+    _fromDate.tintColor = [UIColor blackColor];
     _fromDate.datePickerMode = UIDatePickerModeDate;
     _fromDate.minimumDate = [NSDate date];
     [_fromDate addTarget:self action:@selector(dateChoosen:) forControlEvents:UIControlEventValueChanged];
-    _fromDatePicker = [_fromDate date];
-    [self.placeContainer addSubview:_fromDate];
+
+    [self.placeContainerFrom addSubview:_fromDate];
     
-//    _arrivalButton = [UIButton buttonWithType:UIButtonTypeSystem];
-//    [_arrivalButton setTitle:NSLocalizedString(@"where_field", "") forState:UIControlStateNormal];
-//    _arrivalButton.tintColor = [UIColor blueColor];
-//    _arrivalButton.frame = CGRectMake(30, CGRectGetMaxY(_departureButton.frame) + 20, [UIScreen mainScreen].bounds.size.width - 100.0, 60);
-//    _arrivalButton.layer.cornerRadius = 4.0;
-//    _arrivalButton.backgroundColor = [[UIColor lightGrayColor]colorWithAlphaComponent:0.6];
-//    [_arrivalButton addTarget:self action:@selector(placeButtonTaped:) forControlEvents:UIControlEventTouchUpInside];
-//    [self.placeContainer addSubview:_arrivalButton];
-//
-    [self.view addSubview:_placeContainer];
-//
+    [self.view addSubview:_placeContainerFrom];
+    
+    _placeContainerTo = [[UIView alloc] initWithFrame:CGRectMake(20, CGRectGetMaxY(_placeContainerFrom.frame) + 20, [UIScreen mainScreen].bounds.size.width - 40, 170)];
+    _placeContainerTo.backgroundColor = [[UIColor systemBlueColor]colorWithAlphaComponent:0.3];
+    _placeContainerTo.layer.shadowColor = [[[UIColor blackColor] colorWithAlphaComponent:0.3] CGColor];
+    _placeContainerTo.layer.shadowOffset = CGSizeZero;
+    _placeContainerTo.layer.shadowRadius = 20;
+    _placeContainerTo.layer.shadowOpacity = 1.0;
+    _placeContainerTo.layer.cornerRadius = 6.0;
+    
+    _arrivalButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [_arrivalButton setTitle:NSLocalizedString(@"where_field", "") forState:UIControlStateNormal];
+    _arrivalButton.tintColor = [UIColor blueColor];
+    _arrivalButton.frame = CGRectMake(30, 20, [UIScreen mainScreen].bounds.size.width - 100.0, 60);
+    _arrivalButton.layer.cornerRadius = 4.0;
+    _arrivalButton.backgroundColor = [[UIColor lightGrayColor]colorWithAlphaComponent:0.9];
+    [_arrivalButton addTarget:self action:@selector(placeButtonTaped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.placeContainerTo addSubview:_arrivalButton];
+
+    _toDate = [[UIDatePicker alloc] initWithFrame:CGRectMake(30, CGRectGetMaxY(_arrivalButton.frame) + 20, [UIScreen mainScreen].bounds.size.width - 100.0, 60)];
+    
+    _toDate.backgroundColor = [[UIColor lightGrayColor]colorWithAlphaComponent:0.6];
+    _toDate.tintColor = [UIColor blackColor];
+    _toDate.datePickerMode = UIDatePickerModeDate;
+    _toDate.minimumDate = [NSDate date];
+    _toDate.clipsToBounds = YES;
+    [_toDate addTarget:self action:@selector(dateChoosen:) forControlEvents:UIControlEventValueChanged];
+
+    [self.placeContainerTo addSubview:_toDate];
+    
+    [self.view addSubview:_placeContainerTo];
+
     _searchButton = [UIButton buttonWithType:UIButtonTypeSystem];
     [_searchButton setTitle:NSLocalizedString(@"main_search", "") forState:UIControlStateNormal];
     _searchButton.tintColor = [UIColor blackColor];
-    _searchButton.frame = CGRectMake(30, CGRectGetMaxY(_placeContainer.frame) + 30, [UIScreen mainScreen].bounds.size.width - 60.0, 60);
+    _searchButton.frame = CGRectMake(30, CGRectGetMaxY(_placeContainerTo.frame) + 30, [UIScreen mainScreen].bounds.size.width - 60.0, 60);
     _searchButton.layer.cornerRadius = 4.0;
     _searchButton.titleLabel.font = [UIFont systemFontOfSize:20 weight:UIFontWeightBold];
     _searchButton.backgroundColor = [[UIColor lightGrayColor]colorWithAlphaComponent:0.8];
     [_searchButton addTarget:self action:@selector(searchButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:_searchButton];
     
-    _mapButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    [_mapButton setTitle:@"Search on Map" forState:UIControlStateNormal];
-    _mapButton.tintColor = [UIColor whiteColor];
-    _mapButton.frame = CGRectMake(30, CGRectGetMaxY(_searchButton.frame) + 30, [UIScreen mainScreen].bounds.size.width - 60.0, 60);
-    _mapButton.layer.cornerRadius = 4.0;
-    _mapButton.titleLabel.font = [UIFont systemFontOfSize:20 weight:UIFontWeightBold];
-    _mapButton.backgroundColor = [[UIColor lightGrayColor]colorWithAlphaComponent:0.8];
-    [_mapButton addTarget:self action:@selector(mapButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:_mapButton];
+//    _mapButton = [UIButton buttonWithType:UIButtonTypeSystem];
+//    [_mapButton setTitle:@"Search on Map" forState:UIControlStateNormal];
+//    _mapButton.tintColor = [UIColor whiteColor];
+//    _mapButton.frame = CGRectMake(30, CGRectGetMaxY(_searchButton.frame) + 30, [UIScreen mainScreen].bounds.size.width - 60.0, 60);
+//    _mapButton.layer.cornerRadius = 4.0;
+//    _mapButton.titleLabel.font = [UIFont systemFontOfSize:20 weight:UIFontWeightBold];
+//    _mapButton.backgroundColor = [[UIColor lightGrayColor]colorWithAlphaComponent:0.8];
+//    [_mapButton addTarget:self action:@selector(mapButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+//    [self.view addSubview:_mapButton];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(dataLoaded) name:kDataManagerLoadDataDidConplete object:nil];
 }
@@ -125,11 +149,13 @@
 
 -(void)dateChoosen:(UIDatePicker *)sender {
    
-    NSLog(@"chosen date");
-    NSLog(@"%@",sender.date);
-    NSLog(@"%@",_fromDatePicker);
+    if([sender isEqual:_fromDate]){
+        _searchRequest.departureDate = sender.date;
         
-  
+    } else {
+        _searchRequest.returnDate = sender.date;
+    }
+        [self.view endEditing:YES];
 }
 
 -(void)mapButtonTapped:(UIButton *)sender {
